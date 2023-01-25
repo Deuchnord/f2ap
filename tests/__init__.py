3983f758666c78637e2e13f7f00b7e79e4be8fa2
Jérôme Deuchnord <jerome@deuchnord.fr>
2023-01-25 14:51:59 +0100

    Add webserver tests

---
+++ tests/__init__.py
@@ -0,0 +1,38 @@
+from os import path, remove
+
+from f2ap.config import Configuration
+from f2ap.data import Database
+
+
+PATH_DIRNAME = path.dirname(__file__)
+
+
+def get_fake_db(db_path: str, delete_first: bool = False) -> Database:
+    realpath = f"{PATH_DIRNAME}/{db_path}"
+    if delete_first and path.exists(realpath):
+        remove(realpath)
+
+    return Database(get_fake_config(db_path))
+
+
+def get_fake_config(db_path: str):
+    return Configuration(
+        url="example.com",
+        db=f"{PATH_DIRNAME}/{db_path}",
+        website={
+            "url": "https://example.com/blog",
+            "feed": "https://example.com/blog.feed",
+        },
+        actor={
+            "username": "test",
+            "display_name": "The Test Profile",
+            "summary": "What did you expect?",
+            "followings": ["@coolperson@example.org"],
+            "attachments": {
+                "Website": "https://example.com",
+            },
+            "public_key": f"{PATH_DIRNAME}/files/fake-rsa/public",
+            "private_key": f"{PATH_DIRNAME}/files/fake-rsa/private",
+        },
+        message={"format": ""},
+    )
