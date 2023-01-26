7f5261de357238e58dac9b8eb8256ad5dffd9575
Jérôme Deuchnord <jerome@deuchnord.fr>
2023-01-26 16:58:13 +0100

    Make comments an option

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,5 @@
 from os import path, remove
+from uuid import uuid4
 
 from f2ap.config import Configuration
 from f2ap.data import Database
@@ -7,7 +8,10 @@ from f2ap.data import Database
 PATH_DIRNAME = path.dirname(__file__)
 
 
-def get_fake_db(db_path: str, delete_first: bool = False) -> Database:
+def get_fake_db(db_path: str = None, delete_first: bool = False) -> Database:
+    if db_path is None:
+        db_path = f"/files/database.{uuid4().hex}.db"
+
     realpath = f"{PATH_DIRNAME}/{db_path}"
     if delete_first and path.exists(realpath):
         remove(realpath)
--- tests/activitypub.py
+++ tests/activitypub.py
@@ -1,6 +1,11 @@
+from uuid import uuid4
+from pytest_mock import MockerFixture
+
+from . import get_fake_db
+
 from f2ap import activitypub
 from f2ap.enum import Visibility
-from f2ap.model import W3C_ACTIVITYSTREAMS_PUBLIC
+from f2ap.model import W3C_ACTIVITYSTREAMS_PUBLIC, Note
 
 
 def test_get_note_visibility_public_note():
@@ -58,3 +63,25 @@ def test_get_note_visibility_mentioned_only_note():
             "cc": ["https://example.net/users/anotherUser"],
         }
     )
+
+
+def test_inbox_can_handle_comments(mocker: MockerFixture):
+    db = get_fake_db()
+    comment_uuid = uuid4()
+    mocker.patch.object(db, "insert_comment", return_value=comment_uuid)
+
+    activitypub.handle_inbox()
+
+    replying_to = Note(
+        uuid=comment_uuid,
+        id=42,
+        name=
+    )
+    url = n
+    published_on = n
+    author_url = n
+    content = n
+    visibility = n
+    tags = []
+
+    db.insert_comment.assert_called_with(replying_to, url, published_on, author_url, content, visibility, tags)
